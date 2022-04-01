0e7a8de08d58ebb8c1e5ba0810e1a80574aa64d6
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-04-01 12:57:44 +0530

    trying to omit ssl var

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? true : false
+  connectionString: process.env.DATABASE_URL
 });
 
 
@@ -13,10 +13,11 @@ router
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM test');
-    const results = { 'results': (result) ? result.rows : null};
+    const results = (result) ? result.rows : null;
     res.send(results);
     client.release();
-  } catch (err) {
+  } 
+  catch (err) {
     console.error(err);
     res.send("Error " + err);
   }
