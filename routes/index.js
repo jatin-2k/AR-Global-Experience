e03a2be969362a1620795ba8fca26a700fa2ba56
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-04-01 12:03:12 +0530

    pg

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router
+.get('/', function(req, res, next) {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 module.exports = router;
